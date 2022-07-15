bb1b50be8687963bff77c8b0e1ab0bcbe0976d00
donald.peltier <donald.peltier@nps.edu>
2022-07-14 20:06:56 -0700

    class practice

--- week1/test.py
+++ week1/test.py
@@ -1,3 +1,15 @@
-print("hi")
+class Dog():
+    species="Canis"
+
+    def __init__(self,name,age):
+        self.age = age
+        self.name = name
+    
+    def speak(self,sound):
+        return f"The dog {self.name} makes the sound {self.sound}"
+
+captain=Dog("Captain", 14)
+captain.name
+captain.age
 
 
