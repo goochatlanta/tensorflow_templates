810b7f846a1e3c2b54ab2459761f3e1d6eb541b3
g.andrianopoulos.gr <georgios.andrianopoulos.gr@submit-0.hamming.cluster>
2022-07-22 11:20:15 -0700

    ready to use

--- train_SBATCH_keras.sh
+++ train_SBATCH_keras.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=marko.orescanin
+#SBATCH --job-name=andrianopoulos
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=00:10:00
-#SBATCH --output=./logs_hamming/titans-out-%j.txt
+#SBATCH --output=titans-out-%j.txt
 #SBATCH --partition=beards
 
 . /etc/profile
 
 module load lang/miniconda3/4.10.3
 
-source activate py39_cs4321
+source activate cs4321
 
 python trainer/task.py \
---model_dir="/home/marko.orescanin/data/models/mnist_tests$(date +%Y-%m-%d_%H-%M-%S)/" \
+--model_dir="/home/georgios.andrianopoulos.gr/smallwork/models/mnist_tests$(date +%Y-%m-%d_%H-%M-%S)/" \
 --model_type="fully_connected" \
 --num_epochs=10 \
 --batch_size=10 \
